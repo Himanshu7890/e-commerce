f8ea81e4770460d847e8b798f6816df5dba5262f
Himanshu7890 <ht3702786@gmail.com>
2025-02-08 21:03:16 +0530

    check

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Wlecome to the Shopping To Explore The New Products For your daily Uses</>
+    <>Wlecome to the Shopping To Explore The New Products For your daily Use</>
   );
 }
